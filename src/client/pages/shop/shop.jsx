826490460d34a2dc1e4825d07cc7911023e59e3a
Jill Magsaysay <jill@mondorobot.com>
2017-05-11 15:20:52 -0600

    fixing product image size

--- src/client/pages/shop/shop.jsx
+++ src/client/pages/shop/shop.jsx
@@ -148,8 +148,8 @@ export default class Shop extends Component {
                     <i className="mdl-list__item-avatar">
                       <img
                         src="http://sc01.alicdn.com/kf/HTB1t2fyOpXXXXa7XXXXq6xXFXXXY/Mountain-Bike-types-tangze-disc-brake-price.jpg"
-                        height="4rem"
-                        width="4rem"
+                        height="100%"
+                        width="100%"
                       />
                     </i>
                     <span>{part.name}</span>
