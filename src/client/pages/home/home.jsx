c9b6ac90a0953fd111c2961044fbc00b31db51c2
Jill Magsaysay <jill@mondorobot.com>
2017-05-11 16:12:39 -0600

    updates to homepage

--- src/client/pages/home/home.jsx
+++ src/client/pages/home/home.jsx
@@ -8,28 +8,34 @@ export default class Home extends Component {
 
   render() {
     this.choices = [
-      {'path':'shop', 'icon': 'shopping_cart'},
-      {'path':'ride', 'icon': 'directions_bike'},
-      {'path':'build', 'icon': 'build'},
-      {'path':'notifications', 'icon': 'notifications'},
-      {'path':'face', 'icon': 'face'}
+      {'description': 'Garage', 'path':'garage', 'icon': 'view_array'},
+      {'description': 'Shop', 'path':'shop', 'icon': 'shopping_cart'},
+      {'description': 'Ride', 'path':'ride', 'icon': 'directions_bike'},
+      {'description': 'Build it', 'path':'build', 'icon': 'build'},
+      {'description': 'Stats', 'path':'notifications', 'icon': 'notifications'},
+      {'description': 'Social', 'path':'face', 'icon': 'face'}
     ];
     return (
-      <div className="mdl-list">
-        {this.choices.map((choice, i) => {
-          return (
-            <div className="mdl-list__item" key={`path-${i}`}>
-              <Link
-                to={`/${choice.path}`}
-              >
-                <span className="mdl-list__item-primary-content">
-                  <i className="material-icons mdl-list__item-avatar">{choice.icon}</i>
-                </span>
-              </Link>
-            </div>
-          );
-        })}
-      </div>
+      <section className="home-options">
+        <div className="mdl-list">
+          {this.choices.map((choice, i) => {
+            return (
+              <div className="mdl-list__item" key={`path-${i}`}>
+                <Link
+                  to={`/${choice.path}`}
+                >
+                  <span className="mdl-list__item-primary-content">
+                    <i className="material-icons mdl-list__item-avatar mdl-list_noMarginRight">
+                      {choice.icon}
+                    </i>
+                  </span>
+                  <h6 className="textCenter">{choice.description}</h6>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     );
   }
 }
